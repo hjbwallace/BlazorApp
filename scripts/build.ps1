6d3bad08ee89173171c6dc60ea2ea3bd6d9b64cc
Harrison <hjbwallace@gmail.com>
2019-11-16 07:36:11 +0800

    Adding build and deploy scripts
    * These will be run by a CI/CD pipeline to automatically deploy the code to Azure on a PR merge

---
+++ scripts/build.ps1
@@ -0,0 +1,20 @@
+$srcPath = Join-Path -Path $PSScriptRoot -ChildPath '..\src'
+$artifactsPath = Join-Path -Path $srcPath -ChildPath '..\artifacts'
+
+# Remove any previous artifacts 
+if (Test-path $artifactsPath) { Remove-Item -Recurse -Force $artifactsPath }
+New-Item -Path $artifactsPath -ItemType Directory | Out-Null
+
+$build_version = "1.0.0"
+
+# Build the projects that will not be published
+dotnet clean "$($srcPath)\BlazorApp.sln"
+dotnet restore "$($srcPath)\BlazorApp.sln"
+
+dotnet build "$($srcPath)\BlazorApp\BlazorApp.csproj"
+dotnet build "$($srcPath)\BlazorApp.Client\BlazorApp.Client.csproj"
+
+# Publish the Server project and save the artifacts path
+dotnet publish "$($srcPath)\BlazorApp.Server\BlazorApp.Server.csproj" -c Release --no-restore /p:Version=$build_version
+
+Compress-Archive -Path "$($srcPath)\BlazorApp.Server\bin\Release\netcoreapp3.0\publish\*" -DestinationPath "$($artifactsPath)\BlazorApp.Server.package.zip" -CompressionLevel Optimal -Force
