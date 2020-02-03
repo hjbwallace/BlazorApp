36b4d9e929ff355c94cc5bf9f90a685047ecd1e4
Harrison <hjbwallace@gmail.com>
2020-02-03 22:47:34 +0800

    Updating to Blazor WebAssembly 3.2.0 Preview 1
    https://devblogs.microsoft.com/aspnet/blazor-webassembly-3-2-0-preview-1-release-now-available/
    
    * Version updated to 3.2
    * Simplified startup
    * Updated css and error handling as per the latest templates

--- src/BlazorApp.Client/Program.cs
+++ src/BlazorApp.Client/Program.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using BlazorApp.Client.Security;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace BlazorApp.Client
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddSingleton<ITokenRepository, LocalStorageTokenRepository>();
+
+            // Make the same instance accessible as both AuthenticationStateProvider and ApiAuthenticationStateProvider
+            builder.Services.AddSingleton<ServerAuthenticationStateProvider>();
+            builder.Services.AddSingleton<AuthenticationStateProvider>(provider => provider.GetRequiredService<ServerAuthenticationStateProvider>());
+
+            builder.Services.AddAuthorizationCore(config =>
+            {
+                config.AddPolicy(Policies.IsAdmin, Policies.IsAdminPolicy());
+            });
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
     }
 }
--- src/BlazorApp.Server/Startup.cs
+++ src/BlazorApp.Server/Startup.cs
@@ -73,7 +73,7 @@ namespace BlazorApp.Server
             }
 
             app.UseStaticFiles();
-            app.UseClientSideBlazorFiles<Client.Startup>();
+            app.UseClientSideBlazorFiles<Client.Program>();
 
             app.UseRouting();
 
@@ -83,7 +83,7 @@ namespace BlazorApp.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+                endpoints.MapFallbackToClientSideBlazor<Client.Program>("index.html");
             });
         }
 
